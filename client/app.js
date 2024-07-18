3de74afd813b89fb587b8f2e46fafc4a1e1af209
JF <jarda@jarda.info>
2024-07-18 12:46:13 +0200

    don't disable controls, doesn't work correctly in edge anyway

--- client/app.js
+++ client/app.js
@@ -1,25 +1,20 @@
 import './app.scss'
 
-// post values of all inputs, including disabled ones
-document.getElementById('submit_form').addEventListener('formdata', (e) => {
-    const formData = e.formData;
-
-    for (const input of document.querySelectorAll('input:not([type=radio]), input:checked')) {
-        formData.set(input.name, input.value);
-    }
-});
-
 document.querySelectorAll('[data-use-environment]').forEach((e) => {
     const radio = e.querySelector('input[type=radio]');
-    const inputToEnable = e.querySelector('input[type=text]');
-    const inputToDisable = document.querySelector(`[data-use-environment=${e.dataset.useEnvironment === 'true' ? 'false' : 'true'}] input[type=text]`);
+    const inputToActivate = e.querySelector('input[type=text]');
+    const inputToDeactivate = document.querySelector(`[data-use-environment=${e.dataset.useEnvironment === 'true' ? 'false' : 'true'}] input[type=text]`);
 
     if (radio.checked) {
-        inputToDisable.disabled = true;
+        inputToDeactivate.classList.add('opacity-50');
     }
 
     radio.addEventListener('click', () => {
-        inputToEnable.disabled = false;
-        inputToDisable.disabled = true;
+        inputToActivate.classList.remove('opacity-50');
+        inputToDeactivate.classList.add('opacity-50');
     });
+});
+
+window.addEventListener("pageshow", () => {
+    document.querySelector('[data-use-environment] input[type=radio]:checked').click();
 });
